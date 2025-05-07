6ed9eecdc3f1101ed6b971da66b42cde5db05f23
eamin <md.eamin519@gmail.com>
2025-05-07 22:20:08 +0600

    backend endpoint

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://uni-beez.vercel.app', process.env.FRONTEND_URL || '*'],
+  origin: [process.env.CLIENT_URL, 'https://unibeezzv1.vercel.app/', process.env.FRONTEND_URL || '*'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
